19f91edb2364f483d638f2394ee34d7460dfab02
priyobrato.das <priyodas12@gmail.com>
2025-07-26 21:47:42 +0530

    path param: strict type

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from markdown_it.rules_block import fence
 from scalar_fastapi import scalar_fastapi, get_scalar_api_reference
 import uuid
 from datetime import datetime
@@ -11,9 +12,19 @@ def get_app():
     return {"uuid": uuid.uuid4(), "time": datetime.now()}
 
 
+@app.get("/order/{order_id}")
+def get_order_data(order_id: int):
+    return {
+        "order_id": order_id,
+        "origin_country": "India",
+        "product_description": f"AI TV - {uuid.uuid4()}",
+        "order_placed_at": datetime.now(),
+    }
+
+
 # documentation purpose
 @app.get("/scalar")
-def get_scalar_docs():
+def get_scalar_docs(self):
     return get_scalar_api_reference(
         openapi_url="/openapi.json",
         title=f"Scalar API",
