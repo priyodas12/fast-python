e3e0cc67fe80a41f835708fa9d17042369ee75a6
priyobrato.das <priyodas12@gmail.com>
2025-07-26 21:56:16 +0530

    path param: response validation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from markdown_it.rules_block import fence
 from scalar_fastapi import scalar_fastapi, get_scalar_api_reference
 import uuid
 from datetime import datetime
+from random import random
 
 app = FastAPI()
 
@@ -13,9 +13,10 @@ def get_app():
 
 
 @app.get("/order/{order_id}")
-def get_order_data(order_id: int):
+def get_order_data(order_id: int | str) -> dict[str, str | int | datetime | bool]:
     return {
         "order_id": order_id,
+        "is_available": random() < 0.7,
         "origin_country": "India",
         "product_description": f"AI TV - {uuid.uuid4()}",
         "order_placed_at": datetime.now(),
