723b8da731e5f0d1cad2337c654a046bb150bc9b
priyobrato.das <priyodas12@gmail.com>
2025-07-26 22:18:09 +0530

    random response

--- app/main.py
+++ app/main.py
@@ -3,8 +3,15 @@ from scalar_fastapi import scalar_fastapi, get_scalar_api_reference
 import uuid
 from datetime import datetime
 from random import random
+from faker import *
 
 app = FastAPI()
+
+from faker import Faker
+
+faker = Faker()
+print(faker.name())
+print(faker.email())
 
 
 @app.get("/")
@@ -13,12 +20,15 @@ def get_app():
 
 
 @app.get("/order/{order_id}")
-def get_order_data(order_id: int | str) -> dict[str, str | int | datetime | bool]:
+def get_order_data(
+    order_id: int | str,
+) -> dict[str, str | int | float | bool | datetime]:
     return {
         "order_id": order_id,
         "is_available": random() < 0.7,
         "origin_country": "India",
-        "product_description": f"AI TV - {uuid.uuid4()}",
+        "price": round(random() * 10000, 2),
+        "order_delivery_address": f"{faker.address()}",
         "order_placed_at": datetime.now(),
     }
 
