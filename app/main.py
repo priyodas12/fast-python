48ac0922b278969a437205479a5f65dcbc00e17b
priyobrato.das <priyodas12@gmail.com>
2025-07-28 16:47:11 +0530

    random response - query param

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import logging
 import os
-import sys
 import uuid
 from datetime import datetime
-from http.client import HTTPException
 
+import coloredlogs
 from dotenv import load_dotenv
 from faker import Faker
 from fastapi import FastAPI
+from fastapi.exceptions import ResponseValidationError
 from scalar_fastapi import get_scalar_api_reference
 from sqlalchemy import Boolean, create_engine, DateTime, Float, String, UUID
 from sqlalchemy.dialects import registry
@@ -18,20 +18,17 @@ from sqlalchemy.sql.schema import Column
 app = FastAPI()
 faker = Faker()
 
-logging.basicConfig(
-	# filename="app.log",
-	stream=sys.stdout,
-	level=logging.INFO,
-)
-
 registry.register("cockroach.psycopg2", "cockroach_dialect", "CockroachDBDialect")
 
 load_dotenv(dotenv_path=".env")
 
+log = logging.getLogger("sqlalchemy.engine")
 database_url = os.getenv("COCKROACH_POSTGRESQL_URL")
-logging.info(f"db_url: {database_url}\n\n")
 
-engine = create_engine(database_url, echo=True, future=True, hide_parameters=False)
+coloredlogs.install(level="INFO", logger=log)
+log.info(f"db_url: {database_url}\n\n")
+
+engine = create_engine(database_url, echo=True, future=True, hide_parameters=False, )
 session = sessionmaker(bind=engine, autocommit=False, expire_on_commit=False)
 Base = declarative_base()
 
@@ -53,14 +50,25 @@ class Order(Base):
 	order_discount = Column(Float, nullable=False)
 	customer_id = Column(String, nullable=False)
 
+	def __str__(self):
+		return (f"""\nOrder::
+		        order_id: {self.order_id} ,
+		        delivery_date:: {self.delivery_date},
+		        order_create_date:: {self.order_create_date},
+		        order_availability:: {self.order_availability},
+		        order_price:: {self.order_price},
+		        customer_id:: {self.customer_id},
+		        order_barcode:: {self.order_barcode}
+		        """)
+
 
 def get_db_connection():
 	db = session()
 	try:
-		logging.info(f"connecting to db: {database_url} at {datetime.now()}")
+		log.info(f"connecting to db: {database_url} at {datetime.now()}")
 		yield db
 	except ():
-		logging.error("error  while connecting to db")
+		log.error("error  while connecting to db")
 	finally:
 		db.close()
 
@@ -70,20 +78,45 @@ def get_app():
 	return {"uuid": uuid.uuid4(), "time": datetime.now()}
 
 
-@app.get("/order/{order_id}")
-def get_order_data(order_id: str):
+@app.get("/order/customer", response_model=None)
+def get_order_data(customer_id: str) -> dict[str, Order] | dict[str, str]:
 	try:
 		db = session()
-		order = db.query(Order).filter(Order.order_id == order_id).first()
-		logging.info(f"******************get order {order_id} from db: {order}\n\n")
-		if not order:
-			raise HTTPException(detail="Order not found")
-		return {"order": order}
+		order_orm = db.query(Order).filter(Order.customer_id == str(customer_id)).first()
+		log.info(f"order with customer_id {customer_id} from cockroachdb: {order_orm.__str__()}")
+		if not order_orm:
+			log.debug(f"error while getting order with customer_id {customer_id} from db")
+			return {"error": f"customer_id:: {customer_id} not found"}
+		return {"orders": order_orm}
 	except SQLAlchemyError as e:
-		logging.error(e.args[0])
+		log.error(e.args[0])
 		return {"error": str(e.args[0])}
 	except AssertionError as e:
-		logging.error(e.args[0])
+		log.error(e.args[0])
+		return {"error": str(e.args[0])}
+	except ResponseValidationError as e:
+		log.error(e.args[0])
+		return {"error": str(e.args[0])}
+
+
+@app.get("/order/{order_id}", response_model=None)
+def get_order_data(order_id: str) -> dict[str, Order] | dict[str, str]:
+	try:
+		db = session()
+		order_orm = db.query(Order).filter(Order.order_id == str(order_id)).first()
+		log.info(f"order {order_id} from cockroachdb: {order_orm.__str__()}")
+		if not order_orm:
+			log.debug(f"error while getting order {order_id} from db")
+			return {"error": f"order_id:: {order_id} not found"}
+		return {"orders": order_orm}
+	except SQLAlchemyError as e:
+		log.error(e.args[0])
+		return {"error": str(e.args[0])}
+	except AssertionError as e:
+		log.error(e.args[0])
+		return {"error": str(e.args[0])}
+	except ResponseValidationError as e:
+		log.error(e.args[0])
 		return {"error": str(e.args[0])}
 
 
@@ -92,13 +125,14 @@ def get_order_data_v2():
 	try:
 		db = session()
 		orders = db.query(Order).all()
-		logging.info(f"**************get orders v2 in db: {orders}")
+		for order in orders:
+			log.info(f"fetched orders: {order.__str__()}")
 		return {"orders": orders}
 	except SQLAlchemyError as e:
-		logging.error(e.args[0])
+		log.error(e.args[0])
 		return {"error": str(e.args[0])}
 	except AssertionError as e:
-		logging.error(e.args[0])
+		log.error(e.args[0])
 		return {"error": str(e.args[0])}
 
 
