510efecb44964502262d73d7289ab086d7591440
priyobrato.das <priyodas12@gmail.com>
2025-08-08 14:08:45 +0530

    orm -refactor

--- app/model/order_orm.py
+++ app/model/order_orm.py
@@ -7,27 +7,6 @@ from sqlalchemy.sql.schema import Column
 Base = declarative_base()
 
 
-def order_to_order_orm(order):
-	order_orm = OrderORM()
-	order_orm.order_id = order.order_id
-	order_orm.order_desc = order.order_desc
-	order_orm.order_create_date = order.order_create_date
-	order_orm.order_update_date = order.order_update_date
-	order_orm.delivery_date = order.delivery_date
-	order_orm.order_price = order.order_price
-	order_orm.order_origin = order.order_origin
-	order_orm.order_availability = order.order_availability
-	order_orm.order_barcode = order.order_barcode
-	order_orm.order_discount = order.order_discount
-	order_orm.customer_id = order.customer_id
-	order_orm.carrier_id = order.carrier_id
-	order_orm.weight = order.weight
-	order_orm.volume = order.volume
-	order_orm.package_type = order.package_type
-	order_orm.order_status = order.order_status
-	return order_orm
-
-
 class OrderORM(Base):
 	__tablename__ = "order"
 	__table_args__ = {"schema": "orders"}
